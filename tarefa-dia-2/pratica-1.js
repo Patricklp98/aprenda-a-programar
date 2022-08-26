a3145ff02814a189b28e185064b5e5cdcc4ca348
Patricklp98 <patrickcarvalho198@gmail.com>
2022-08-26 19:35:47 -0300

    valor

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,17 +1,25 @@
-let nome = "Patrick"
-console.log(nome)
+let nome 
 
-let email = "patrickcarvalho198@gmail.com"
-console.log(email)
+let email 
 
-let profissao = "nenhuma"
-console.log(profissao)
+let profissao 
 
-let idade = "23"
-console.log(idade)
+let idade 
 
-let lugardossonhos= "EUA"
-console.log(lugardossonhos)
+let lugardossonhos
+
+nome = "Patrick"
+email = "patrickcarvalho198@gmail.com"
+profissao = "Rockeiro"
+idade = "23"
+lugardossonhos = "EUA"
+
+console.log(Olá, ' ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para os " + lugardossonhos + ". Confirma pra gente alguns dados? A sua idade é  " + idade + ", você é " + profissão + " e o seu email é " + email + "? Ficamos no aguardo. Muito obrigado e boa viagem! :D
+
+
+
+
+
 
 
 
